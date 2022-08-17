503d89c44975195305a9b63914b24d381ee2559d
jiten <jpalaparthi@gmail.com>
2022-08-17 14:38:33 +0530

    Add array

--- 10-arrays/main.go
+++ 10-arrays/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"reflect"
 )
 
 func main() {
@@ -17,12 +18,39 @@ func main() {
 	for i := 0; i < len(arr1); i++ {
 		arr1[i] = rand.Intn(1000)
 	}
+
 	fmt.Println(arr1)
 	arr2 := [5]int{10, 4, 9, 10, 15} // short hand declaration
+	arr4 := [5]int{}                 // deep copy, value copy
+	arr4 = arr2
+	arr2[0] = 100
+	fmt.Println("Type of arr2:", reflect.TypeOf(arr2))
 	fmt.Println(arr2)
+	fmt.Println("Type of arr4:", reflect.TypeOf(arr4))
+	fmt.Println(arr4)
 	//[...] --> this must be evalauated at compile time
 	arr3 := [...]int{10, 14, 15, 16, 18, 19, 20, 1, 5, 34, 72, 21} // shorthand declaration
 	fmt.Println(arr3)
 	fmt.Println("Length of an array:", len(arr3))
 	fmt.Println("Capacity of an array:", cap(arr3))
+	for _, val := range arr3 {
+		print(val, " ")
+	}
+
+	err := changeVal(arr3, 3, 116)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(arr3)
+}
+
+// nil can be used for pointers, slice, map, chan and interface,interface{}
+func changeVal(arr [12]int, i uint, v int) error {
+	if int(i) >= len(arr) {
+		//return errors.New("invalid index")
+		return fmt.Errorf("invald index")
+	}
+	arr[i] = v
+	fmt.Println("Inside function", arr)
+	return nil
 }
---
+++ 11-arrays/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr1 := [2][3]int{{1, 2, 3}, {4, 5, 6}}
+	fmt.Println(arr1)
+
+	for i := 0; i < len(arr1); i++ {
+		for j := 0; j < len(arr1[i]); j++ {
+			print(arr1[i][j], " ")
+		}
+		println()
+	}
+
+	arr2 := [2][2][3]int{{{1, 2, 3}, {4, 5, 6}}, {{7, 8, 9}, {10, 11, 12}}}
+	fmt.Println(arr2)
+
+	for i := 0; i < len(arr2); i++ {
+		for j := 0; j < len(arr2[i]); j++ {
+			for k := 0; k < len(arr2[i][j]); k++ {
+				print(arr2[i][j][k], " ")
+			}
+		}
+		println()
+	}
+
+}
+
+// Reverse an array
+// find max and min elements from an array
+// find the sum of elements in an array
